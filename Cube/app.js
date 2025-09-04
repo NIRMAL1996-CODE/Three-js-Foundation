3075128abf6a54351da6d5828e99f47b813d4188
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-09-04 23:34:39 +0530

    CUBE

---
+++ Cube/app.js
@@ -0,0 +1,31 @@
+import * as THREE from 'three';
+
+// 1. Scene: just makes the empty stage.Without a scene, Three.js doesn’t know where to put objects or lights.
+const scene = new THREE.Scene();
+
+// 2. Camera = eyes (tells Three.js what to display and from which angle).
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+// 3. Renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// 4. Cube
+//to create a cube we gave this class BoxGeometry "box".
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color:"white"});
+const cube = new THREE.Mesh(geometry, material);
+cube.position.z = 2;
+
+scene.add(cube);
+
+// 5. Animate
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+animate();
