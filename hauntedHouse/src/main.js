653e5779c18daf8d428e99918fd44693b4c52632
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-11-19 22:00:38 +0530

    ghost house

--- hauntedHouse/src/main.js
+++ hauntedHouse/src/main.js
@@ -11,24 +11,36 @@ camera.lookAt(0, 1, 0);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setClearColor("#262837")
 document.body.appendChild(renderer.domElement);
 
 const controls= new OrbitControls(camera, renderer.domElement);
 controls.enableDamping=true;
 
-const light = new THREE.AmbientLight("white", 1  ); // soft white light
-light.position.set(2,2,2)
-scene.add( light );
-
-const directional = new THREE.DirectionalLight("white",2);
-directional.position.set(5, 5, 5);
-scene.add(directional);
+const fog = new THREE.Fog("#262837", 1, 15);
+scene.fog= fog;
 
 const house = new THREE.Group();
 scene.add(house);
 
+const light = new THREE.AmbientLight("#0d0f26", 0.5);
+light.position.set(2,2,2)
+scene.add( light );
+
+const directional = new THREE.DirectionalLight("#3a4cfe", 0.6);
+directional.position.set(4, 5, -2);
+scene.add(directional);
+
+const doorLight = new THREE.PointLight("#ff4b1f", 3.5, 7); 
+doorLight.position.set(0, 2.4, 2.4); // front of door
+scene.add(doorLight);
+
+const texture =new THREE.TextureLoader();
+
+
+
 const geometry = new THREE.PlaneGeometry(20,20);
-const material = new THREE.MeshStandardMaterial({ color:"#a8e6a1",side: THREE.DoubleSide });
+const material = new THREE.MeshStandardMaterial({ color:"#a8e6a1"});
 const floor = new THREE.Mesh(geometry, material);
 floor.rotation.x = -Math.PI /2; 
 floor.position.y = -0.01; 
@@ -57,20 +69,41 @@ house.add(door);
 const bushgeometry= new THREE.SphereGeometry(0.5, 16, 16);
 const bushMaterial= new THREE.MeshStandardMaterial({color: "#2e8b57" });
 const bush1 = new THREE.Mesh(bushgeometry,bushMaterial);
-bush1.position.set(-1.6, 0.5, 3.5); 
+bush1.position.set(-1.6, 0.2, 2.3); 
 bush1.scale.set(0.5,0.5,0.5)
 
 const bush2 = new THREE.Mesh(bushgeometry,bushMaterial);
-bush2.position.set(-1, 0.5, 3.5); 
+bush2.position.set(-1, 0.2, 2.3); 
 
 const bush3 = new THREE.Mesh(bushgeometry,bushMaterial);
-bush3.position.set(1.6, 0.5, 3.5); 
+bush3.position.set(1.6, 0.2, 2.3); 
 bush3.scale.set(0.5,0.5,0.5)
 
 const bush4 = new THREE.Mesh(bushgeometry,bushMaterial);
-bush4.position.set(1, 0.5, 3.5); 
+bush4.position.set(1, 0.2, 2.3); 
 
 house.add(bush1,bush2,bush3,bush4);
+
+//grave
+const graves =new THREE.Group();
+scene.add(graves);
+
+ const graveGeo = new THREE.BoxGeometry(0.6, 0.8, 0.2);
+  const graveMat = new THREE.MeshStandardMaterial({ color: "#aaaaaa" });
+
+  for(let i=0; i<50; i++){
+    const angle =Math.random()* Math.PI *2;
+    const radius =3 + Math.random() *6
+    const x =Math.sin(angle) * radius;
+    const z =Math.cos(angle) * radius;
+  
+  const grave = new THREE.Mesh(graveGeo, graveMat);
+  grave.position.set(x, 0.3, z);
+  grave.rotation.y=(Math.random() -0.5 )*0.4;
+  grave.rotation.z=(Math.random() -0.5 )*0.4;
+  graves.add(grave);
+}
+  
 
 
 
