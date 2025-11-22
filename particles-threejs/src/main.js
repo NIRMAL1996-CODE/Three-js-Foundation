04aa0f0cb3560f0b9aff6e6cb97647a5f27a3cb9
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-11-22 15:09:20 +0530

    updated

--- particles-threejs/src/main.js
+++ particles-threejs/src/main.js
@@ -7,17 +7,15 @@ const scene = new THREE.Scene();
 
 // camera
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 100);
-camera.position.z = 3;
+camera.position.z = 4;
 
 // renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.antialias = true; 
 document.body.appendChild(renderer.domElement);
 
-// cube
-const geosphere = new THREE.SphereGeometry(1,32,32);
-const geotorus = new THREE.TorusGeometry(1,0.3,32,100);
 // const geocone = new THREE.ConeGeometry(1,2,32);
 // const geoico = new THREE.IcosahedronGeometry(1, 1);
 // const geocylinder = new THREE.CylinderGeometry(1,1,2,32);
@@ -25,12 +23,14 @@ const geotorus = new THREE.TorusGeometry(1,0.3,32,100);
 // const geoplane = new THREE.PlaneGeometry(2,2);
 // const geobox = new THREE.BoxGeometry(1,1,1);
 
-// const positions = new Float32Array([]);
-// geo.setAttribute("position", new THREE.BufferAttribute(positions, 3));
-const mat = new THREE.PointsMaterial({ size: 0.01,sizeAttenuation:true });
-const sphere = new THREE.Points(geosphere, mat);
-sphere.material.color.set(0x0000ff);
-const torus = new THREE.Points(geotorus, mat);
+const geosphere = new THREE.SphereGeometry(2,64,64);
+const matsphere = new THREE.PointsMaterial({ size: 0.01,sizeAttenuation:true,  alphaTest: 0.5, transparent: true, opacity: 0.95 });
+const sphere = new THREE.Points(geosphere, matsphere );
+sphere.material.color.set("white");
+
+const geotorus = new THREE.TorusGeometry(1,0.4,64,200);
+const mattorus = new THREE.PointsMaterial({ size: 0.01,sizeAttenuation:true,alphaTest: 0.4,transparent: true,opacity: 0.95 });
+const torus = new THREE.Points(geotorus, mattorus);
 torus.material.color.set("pink");
 // const cone = new THREE.Points(geocone, mat);
 // const box = new THREE.Points(geobox, mat);
@@ -39,9 +39,8 @@ torus.material.color.set("pink");
 // const ico = new THREE.Points(geoico, mat);
 // const circle = new THREE.Points(geocircle, mat);
 
-sphere.position.set( 0, 0, -1 );
-torus.position.set( 0, 0, 1 );
-
+sphere.position.set( -2.5, 0, -1 );
+torus.position.set( 1.5, 0, 1 );
 scene.add(sphere,torus);
 
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -58,8 +57,10 @@ window.addEventListener("resize", () => {
 function animate() {
   controls.update();
   requestAnimationFrame(animate);
-  sphere.rotation.x+=0.01
-  torus.rotation.y+=0.01
+  // sphere.rotation.x+=0.01
+  // torus.rotation.y+=0.01
+  sphere.rotation.x += 0.005;
+  torus.rotation.y += 0.005;
   renderer.render(scene, camera);
 }
 animate();
