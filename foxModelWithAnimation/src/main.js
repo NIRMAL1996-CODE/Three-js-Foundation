59e422f198f9cc7b35f26e94c39fc01f7b1eaa55
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-12-13 20:25:40 +0530

    updated

--- foxModelWithAnimation/src/main.js
+++ foxModelWithAnimation/src/main.js
@@ -2,7 +2,6 @@ import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader} from "three/addons/loaders/GLTFLoader.js";
-import { AnimationMixer } from 'three';
 
 const scene = new THREE.Scene();
 
